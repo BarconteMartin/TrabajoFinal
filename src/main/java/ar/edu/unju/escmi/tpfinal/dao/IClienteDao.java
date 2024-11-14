8208d1ffe509d25310e663821d14aae5956006e7
wiber <wiber@DESKTOP-29TD6KD>
2024-11-14 14:56:16 -0300

    Todo Clientes y Salon - AltamiranoLara

--- src/main/java/ar/edu/unju/escmi/tpfinal/dao/IClienteDao.java
+++ src/main/java/ar/edu/unju/escmi/tpfinal/dao/IClienteDao.java
@@ -1,5 +1,10 @@
 package ar.edu.unju.escmi.tpfinal.dao;
 
-public class IClienteDao {
+import ar.edu.unju.escmi.tpfinal.entities.Cliente;
 
+public interface IClienteDao {
+	public void guardarCliente(Cliente cliente);
+	public void modificarCliente(Cliente cliente);
+	public Cliente buscarClientePorId(Long idCliente);
+	public void mostrarTodosLosClientes();
 }
